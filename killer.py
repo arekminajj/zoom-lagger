dc8faf76c3a15997746cfea3fa05c1025fd133f2
Arkadiusz Cios <arek123113@gmail.com>
2021-03-10 18:12:30 +0100

    Add process id checker

--- killer.py
+++ killer.py
@@ -1,7 +1,26 @@
 import psutil
 import time
-somepid = 22108
-p = psutil.Process(somepid)
-p.suspend()
-time.sleep(5)
-p.resume()
+
+def pauseAndResumeProcess(process_pid):
+    p = psutil.Process(process_pid)
+    p.suspend()
+    time.sleep(3)
+    p.resume()
+
+def findProcessIdByName(processName):
+    listOfProcessObjects = []
+    for proc in psutil.process_iter():
+       try:
+           pinfo = proc.as_dict(attrs=['pid', 'name', 'create_time'])
+           if processName.lower() in pinfo['name'].lower() :
+               listOfProcessObjects.append(pinfo)
+       except (psutil.NoSuchProcess, psutil.AccessDenied , psutil.ZombieProcess) :
+           pass
+    return listOfProcessObjects;
+
+while True: 
+    processes = findProcessIdByName('zoom')
+    for process in processes:
+        pauseAndResumeProcess(process['pid'])
+
+
